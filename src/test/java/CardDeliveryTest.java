cad3245c8a34483ce61a5e707c478d4818047fee
Краснова Татьяна <redtanya@yandex.ru>
2022-08-11 15:30:50 +0300

    внесение правок

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -2,6 +2,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 public class CardDeliveryTest {
